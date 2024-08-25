d7f42d9c4bb3fc1945036988a81fe3a6623f7239
renganomics <orengasi@gmail.com>
2024-08-25 18:52:25 +0100

    Initial Commit: Created scrape method to read page source

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import selectorlib
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/39.0.2171.95 Safari/537.36'}
+
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url, HEADERS)
+    source = response.text
+    return source
+
+
+if __name__ == "__main__":
+    print(scrape(URL))
