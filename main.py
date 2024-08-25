6b9733e74157d2b008125559323c83f3f4d1bdba
renganomics <orengasi@gmail.com>
2024-08-25 19:45:49 +0100

    Created store and read methods

--- main.py
+++ main.py
@@ -21,7 +21,27 @@ def extract(source):
     return value
 
 
+def send_email():
+    print("Email was sent!")
+
+
+def store(extracted):
+    with open("data.txt", 'a') as file:
+        file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open("data.txt", 'r') as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
+    content = read(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
+
